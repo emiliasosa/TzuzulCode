5d04f346f2aae624c47bf7c60bc84964055695a8
emiliasosa <emi.sosato@gmail.com>
2022-02-15 20:34:10 -0300

    Create index.js

---
+++ Coding Fundamentals/index.js
@@ -0,0 +1,22 @@
+/*let fila = parseInt(prompt("Ingrese numero de filas"))
+let columnas = parseInt(prompt("Ingrese numero de columnas"))*/
+const arr = [0];
+
+/*for(let i = 0; i < fila; i++){
+    let numeros = [];
+    for (let j =0; j<  columnas; j++) {
+       numeros[j] = parseInt(prompt("Ingrese numeros"))
+    }
+    arr[i] = numeros
+}
+*/
+
+let n = 5
+let b = n-1
+let a = 0 
+
+for(let i = a; i <= b ; i++){
+     arr[a][i] = i++ + " "
+}
+
+
