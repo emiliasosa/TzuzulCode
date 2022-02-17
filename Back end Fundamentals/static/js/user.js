abc7b723dbea43b74a18978fe648d9bd3f5dbd26
emiliasosa <emi.sosato@gmail.com>
2022-02-16 22:51:08 -0300

    Update user.js

--- Back end Fundamentals/static/js/user.js
+++ Back end Fundamentals/static/js/user.js
@@ -1,22 +1,65 @@
+let users = document.getElementById('show')
+let userData = []
+
 fetch('/api/users')
 .then((res) => {
     return res.json()})
 .then((data) => {
-    let users = document.getElementById('show')
-    for(let user of data){
-        console.log(user)
+    userData = data
+    renderUsers()
+})
+
+function eliminar(id){
+    fetch("/api/users/"+id, { method:"DELETE" })
+    .then((res) => {return res.json})
+    .then((data) => {
+        /*userData = data 
+        filterUser(id)
+        renderUsers()*/
+        window.location.reload()
+    })
+}
+
+function editar(id){
+    window.location.href = "/edit";
+    fetch("/edit/"+id, { method:"POST" })
+    .then((res) => {return res.json})
+    .then((data) => { window.location.href = "/";})
+}
+
+/*function filterUser(){
+    let newUsers = []
+
+    for(let u of userData){
+        if(u.id !== id){
+            userData.push(u)
+        }
+    }
+    userData = newUsers
+}*/
+
+function renderUsers(){
+    let year = new Date().getFullYear();
+    users.innerHTML = ""
+    for(let user of userData){
+        let edad = year - parseInt(user.birthday)
         users.innerHTML = users.innerHTML + `
         <div class="alluser">
+            <img src='${user.picture}' class="imgcard">
             <p class="nombre">${user.name}</p>
-            <p>${user.birthday}</p>
-            <p>${user.gender}</p>
-            <p>${user.profession}</p>
-            <p>${user.salary}</p>
+            <p class="textcard"><b>Edad:</b> ${edad}</p>
+            <p class="textcard"><b>Genero:</b> ${user.gender}</p>
+            <p class="textcard"><b>Profesión:</b> ${user.profession}</p>
+            <p class="textcard"><b>Salario:</b> $ ${user.salary}</p>
+            <p class="finaltext"><b>Hobby:</b> ${user.about}</p>
             <div class="btndiv">
-            <button class="btnup">Modificar</button>
-            <button onclick='userController.delete(${user.id})' class="btndel">Eliminar</button>
+                <div class="btndivhijo">
+                    <button onclick="editar(${user.id})" class="btnup"> Modificar</button>
+                    <button onclick="eliminar(${user.id})" class="btndel">Eliminar</button>
+                </div>
             </div>
         </div>
     `
     }
-})
+}
+
