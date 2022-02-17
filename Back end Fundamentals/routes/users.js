f1eaf74c265836a76a5bee091c7658700a5d66d9
emiliasosa <emi.sosato@gmail.com>
2022-02-17 11:00:43 -0300

    Update users.js

--- Back end Fundamentals/routes/users.js
+++ Back end Fundamentals/routes/users.js
@@ -40,12 +40,15 @@ router.get('/edit', (req, res)=>{
     return res.sendFile(path.join(__dirname, '../views/edit.html'))
 })
 
-router.put('/edit/:id', async (req, res)=>{
+router.post('/edit/:id', async (req, res)=>{
     const id = req.params.id
-    const user = req.body.user
+    const user = req.body
     const result = await userController.update(user,id)
-    return console.log(result)
+    console.log(user)
+    console.log(result)
+    return res.redirect('/')
     
 })
+
 
 module.exports = router
